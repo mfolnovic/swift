ec9eefc84732b6e633a12cb61560cf2832879faf
Matija Folnovic <matijafolnovic@gmail.com>
2010-06-24 20:14:28 +0200

    huge optimizations on controller execution

--- lib/controller.php
+++ lib/controller.php
@@ -5,31 +5,29 @@ class Controller extends Base {
 	var $globals = array(), $flash = array();
 	
 	function run( $r ) {
-		$this -> controller = $r[ 'controller' ];
-		$this -> action = $r[ 'action' ];
+		$this -> controller = $controller = $r[ 'controller' ];
+		$this -> action = $action = $r[ 'action' ];
 		
 		unset( $r[ 'controller' ], $r[ 'action' ] );
 		$this -> data = $r;
 
-		$this -> get( $this -> controller );
-		$this -> call( $this -> controller, $this -> action );
-	}
-	
-	private function call( $controller, $action ) {		
-		if( is_callable( array( $controller . 'Controller', $action ) ) ) {
-			$str = $controller . 'Controller';
-			$this -> obj = new $str;
-			$this -> obj -> initObj();
-			$this -> obj -> $action();
-		}
-	}
-	
-	private function get( $controller ) {
 		$path = CONTROLLERS_DIR . $controller . ".php";
 		if( file_exists( $path ) )
 			include_once $path;
 		else
 			$this -> render404();
+
+		if( is_callable( array( $tmp = $controller . 'Controller', $action ) ) ) {
+			$this -> obj = new $tmp;
+			$this -> obj -> initObj();
+			$this -> obj -> $action();
+		}
+	}
+	
+	private function call2( $controller, $action ) {		
+	}
+	
+	private function get( $controller ) {
 	}
 	
 	function isAjax() {
