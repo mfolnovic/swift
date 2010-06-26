52041fff9390bdeb7e22ebb3aa9a620d4331ae04
Matija Folnovic <matijafolnovic@gmail.com>
2010-06-26 20:23:58 +0200

    check if apc is available

--- lib/constants.php
+++ lib/constants.php
@@ -1,9 +1,6 @@
 <?php
 
-apc_load_constants( 'PRF' . DIR );
-
-if( !defined( 'LOG_DIR' ) ) {
-	apc_define_constants( 'PRF' . DIR, array (
+$constants = array (
 			'LOG_DIR' => DIR . '/log/',
 			'CONFIG_DIR' => DIR . '/config/',
 			'APP_DIR' => DIR . '/app/',
@@ -13,8 +10,16 @@ if( !defined( 'LOG_DIR' ) ) {
 			'MODEL_DIR' => DIR . '/app/models/',
 			'VIEWS_DIR' => DIR . '/app/views/',
 			'LOG_ERROR' => 'error', // ?
-		)
-	);
+		);
+		
+if( !extension_loaded( 'apc' ) ) {
+	foreach( $constants as $id => $val )
+		define( $id, $val ); 
+} else {
+	apc_load_constants( 'PRF' . DIR );
+
+	if( !defined( 'LOG_DIR' ) )
+		apc_define_constants( 'PRF' . DIR, $constants );
 }
 
 /*
