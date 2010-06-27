4d3f2e3899e4c7bb0c617d30f2a7b2ff9c3e8afa
Matija Folnovic <matijafolnovic@gmail.com>
2010-06-27 23:23:24 +0200

    added 500.php to blog (demo)

---
+++ demo/blog/public/500.php
@@ -0,0 +1,11 @@
+<h1>There was an error!</h1>
+<h2>Message:</h2>
+<p><?php echo $message; ?></p>
+<p>In file <?php echo $file; ?> (line <?php echo $line; ?>)
+<h2>Backtrace:</h2>
+<table>
+<th>#<th>File<th>Line<th>Called
+<?php foreach( $backtrace as $id => $row ) { ?>
+<tr><td><?php echo $id; ?><td><?php echo isset( $row[ "file" ] ) ? $row[ "file" ] : ''; ?><td><?php echo isset( $row[ "line" ] ) ? $row[ 'line' ] : ''; ?><td><?php echo isset( $row[ "function" ] ) ? $row[ "function" ] : ""; ?>
+<? } ?>
+</table>
