f10fe5100945c09968ac69f17031e11f0aec9980
Matija Folnovic <matijafolnovic@gmail.com>
2010-07-01 15:48:44 +0200

    Added ldap support, but should make it database driver, so it can be used in models, but need to refactor model a bit first

--- lib/boot.php
+++ lib/boot.php
@@ -18,6 +18,8 @@ include LIB_DIR . "controller/base.php";
 include LIB_DIR . "controller/controller.php";
 include LIB_DIR . "cache/" . ( $config -> options[ 'cache' ][ 'driver' ] ) . ".php";
 include LIB_DIR . "db/" . ( $config -> options[ 'database' ][ 'default' ][ 'driver' ] ) . ".php";
+include LIB_DIR . "ldap/ldap.php";
+exit;
 include LIB_DIR . "model/model.php";
 include LIB_DIR . "view/view.php";
 
