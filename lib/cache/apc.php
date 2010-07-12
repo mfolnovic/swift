d62106d093e6db92ea1d290530f7de46448ef4aa
Matija Folnovic <matijafolnovic@gmail.com>
2010-07-13 00:02:03 +0200

    Added exists method in apc cache

--- lib/cache/apc.php
+++ lib/cache/apc.php
@@ -3,6 +3,8 @@
 class Cache extends Base {
 	static function set( $index, $value ) {
 		apc_store( $index, $value );
+		
+		return $value;
 	}
 	
 	static function get( $index )  {
@@ -11,6 +13,10 @@ class Cache extends Base {
 	
 	static function delete( $index ) {
 		apc_delete( $index );
+	}
+	
+	static function exists( $index ) {
+		return apc_exists( $index );
 	}
 }
 
