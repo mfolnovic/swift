b2da7a12ac1db909ba2a190ece07a841e15a8c28
Matija Folnovic <matijafolnovic@gmail.com>
2010-07-26 16:33:12 +0200

    More tests on apc, like deleting, setting to array of object etc

--- tests/units/cache/apc.php
+++ tests/units/cache/apc.php
@@ -1,15 +1,40 @@
 <?php
 
+class TempObject {
+	var $data = array();
+	
+	function __construct( $data ) {
+		$this -> data = $data;
+	}
+}
+
 class apcTest extends TestCase {
-	function test_setter() {
+	function test_clear() {
 		$apc = Cache::getInstance( 'apc' );
 		$apc -> clear(); // Just to make sure tests run cleanly
 
 		$this -> assert( !$apc -> exists( 'foo' ), "Foo shouldn't exist since I didn't make it anywhere" );
-	
+	}
+
+	function test_set() {
+		$apc = Cache::getInstance( 'apc' );
+
 		$apc -> set( 'foo', 'bar' );
 		$this -> assertEqual( $apc -> get( 'foo' ), 'bar' );
 		
+		$array = array( 1 => 'hello', '2' => 'world' );
+		$apc -> set( 'foo2', $array );
+		$this -> assertEqual( $apc -> get( 'foo2' ), $array );
+		
+		$array2 = array( new TempObject( $array ), new TempObject( array_reverse( $array ) ) );
+		$apc -> set( 'foo3', $array2 );
+		$this -> assertEqual( $apc -> get( 'foo3' ), $array2 );
+		
+	}
+	
+	function test_delete() {
+		$apc = Cache::getInstance( 'apc' );
+	
 		$apc -> delete( 'foo' );
 		$this -> assert( !$apc -> exists( 'foo' ), "Foo exists, but it should exist" );
 	}
