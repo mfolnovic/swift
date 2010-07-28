f0a8d12f09fd41778ff5d36dfd73fae43558d87b
Matija Folnovic <matijafolnovic@gmail.com>
2010-07-28 19:09:21 +0200

    Fixed __construct in cache

--- lib/cache/cache.php
+++ lib/cache/cache.php
@@ -5,8 +5,8 @@ class Cache {
 	
 	static function loadDrivers( $config ) {
 		foreach( $config as $driver => $options ) {
-			include LIB_DIR . "/cache/$driver.php";
-			$name = "Cache_" . ucfirst( $driver );
+			include LIB_DIR . "/cache/{$options[ 'driver' ]}.php";
+			$name = "Cache_" . ucfirst( $options[ 'driver' ] );
 			self::$instances[ $driver ] = new $name( $options );
 		}
 	}
