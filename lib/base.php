b6fc6132ca697f88aff002ac2222ffd022f0d749
Matija Folnovic <matijafolnovic@gmail.com>
2010-07-30 15:42:41 +0200

    Support before_filter and after_filter in ALL classes

--- lib/base.php
+++ lib/base.php
@@ -1,7 +1,26 @@
 <?php
 
 class Base {
+	var $before_filters = array();
+	var $after_filters  = array();
 	
+	function __construct() {
+		foreach( $this -> before_filters as $function )
+			call_user_func( array( $this, $function ) );
+	}
+	
+	function __destruct() {
+		foreach( $this -> after_filters as $function )
+			call_user_func( array( $this, $func ) );
+	}
+	
+	function before_filter( $function ) {
+		$this -> before_filters[] = $function;
+	}
+	
+	function after_filter( $function ) {
+		$this -> after_filters[] = $function;
+	}
 }
 
 ?>
