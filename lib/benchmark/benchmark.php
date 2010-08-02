a3e00a3f04df5234e2e348b217ab914131cd9b65
Matija Folnovic <matijafolnovic@gmail.com>
2010-08-02 16:00:52 +0200

    Written documentation for Benchmark

--- lib/benchmark/benchmark.php
+++ lib/benchmark/benchmark.php
@@ -1,12 +1,47 @@
 <?php
 
+/**
+ * Swift framework
+ *
+ * @package		Swift
+ * @author		Swift dev team
+ * @copyright	Copyright (c) 2010, Swift dev team
+ * @license		LICENSE
+ */
+
+/**
+ * Swift Benchmark Class
+ *
+ * This class allows you to measure time needed for certain
+ * part of code to run
+ *
+ * @package			Swift
+ * @subpackage	Benchmark
+ * @author			Swift dev team
+ */
+
 class Benchmark extends Base {
 	static $times = array();
-	
+
+	/**
+	 * This function marks start point for name $name
+	 * @access	public
+	 * @static
+	 * @param		string	Mark name
+	 * @return	void
+	 */
 	static function start( $name ) {
 		self::$times[ $name ] = microtime( true );
 	}
-	
+
+	/**
+	 * This function calculates difference between time from name $name
+	 * to current time and writes it to log.
+	 * @access	public
+	 * @static
+	 * @param		string	Mark name
+	 * @return	void
+	 */
 	static function end( $name ) {
 		Log::getInstance() -> write( "[Benchmark] $name: " . round( ( microtime( true ) - self::$times[ $name ] ), 4 ) . " seconds!" );
 	}
