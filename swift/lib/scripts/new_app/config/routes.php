2236107596dec03e897d4e65f0c11056ed5b2f42
Matija Folnovic <matijafolnovic@gmail.com>
2010-08-18 13:05:11 +0200

    remove new_app from script

--- swift/lib/scripts/new_app/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Default route
-$router -> addRoute( "%controller%(/%action(/%id))", array( 'controller' => 'default controller', 'action' => 'default action' ) );
-
-?>
--- swift/lib/scripts/new_app/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define( "LIB_DIR", "../PRF/lib/" );
-define( "DIR", getcwd() );
-
-include LIB_DIR . "boot.php";
-
-?>
