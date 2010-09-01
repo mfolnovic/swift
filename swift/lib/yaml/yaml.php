0a1a4657d9d1f915218a5bef5c13e67a1e53298b
Matija Folnovic <matijafolnovic@gmail.com>
2010-09-01 11:41:13 +0200

    documentation for yaml

--- swift/lib/yaml/yaml.php
+++ swift/lib/yaml/yaml.php
@@ -1,6 +1,32 @@
 <?php
 
+/**
+ * Swift framework
+ *
+ * @author    Swift dev team
+ * @copyright Copyright (c) 2010, Swift dev team
+ * @license   LICENSE
+ * @package   Swift
+ */
+
+/**
+ * Swift Yaml Class
+ *
+ * This class is used for parsing yaml and compiling array to yaml
+ *
+ * @author      Swift dev team
+ * @package     Swift
+ * @subpackage  Yaml
+ */
+
 class Yaml {
+	/**
+	 * Used for parsing yaml files
+	 * @access public
+	 * @param  string $path Path to file
+	 * @return object
+	 * @static
+	 */
 	static function parse( $path ) {
 		return yaml_parse_file( $path );
 	}
