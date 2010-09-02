c223b63ff71c9383d337b07fc0e5f81023fc2313
Matija Folnovic <matijafolnovic@gmail.com>
2010-09-02 10:46:59 +0200

    better docs in db

--- swift/lib/db/db.php
+++ swift/lib/db/db.php
@@ -3,39 +3,46 @@
 /**
  * Swift
  *
- * @package		Swift
  * @author		Swift dev team
  * @copyright	Copyright (c) 2010, Swift dev team
  * @license		LICENSE
+ * @package		Swift
  */
 
 /**
  * Swift Database Class
  *
- * This class works as singleton class for all cache adapters
+ * This class works as factory class for all cache adapters
  *
+ * @author			Swift dev team
  * @package			Swift
  * @subpackage	Database
- * @author			Swift dev team
  */
 
 class Db extends Base {
-	static $instances = array();
+	/**
+	 * This array contains all factory instances of each database adapters
+	 * application tried to use.
+	 */
+	static $adapters = array();
 
 	/**
 	 * Returns factory instance of adapter $adapter
-	 * @access	public
-	 * @return	object
+	 *
+	 * @access public
+	 * @param  string $adapter Adapter name
+	 * @return object
+	 * @static
 	 */
 	static function factory( $adapter ) {
-		if( !isset( self::$instances[ $adapter ] ) ) {
-			$conf    = Config::instance() -> get( 'database', $adapter );
-			$adapter = 'Db_' . ucfirst( $conf[ 'adapter' ] );
+		if( !isset( self::$adapters[ $adapter ] ) ) {
+			$options = Config::instance() -> get( 'database', $adapter );
+			$adapter = 'Db_' . ucfirst( $options[ 'adapter' ] );
 
-			self::$instances[ $adapter ] = new $adapter( $conf );
+			self::$adapters[ $adapter ] = new $adapter( $options );
 		}
 
-		return self::$instances[ $adapter ];
+		return self::$adapters[ $adapter ];
 	}
 }
 
