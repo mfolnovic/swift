c5a091b6c429acc9e26eaa3c2793bfe284aecb6f
Matija Folnovic <matijafolnovic@gmail.com>
2010-09-02 16:26:20 +0200

    better docs - errors

--- swift/lib/errors.php
+++ swift/lib/errors.php
@@ -20,15 +20,21 @@
  */
 
 class Errors extends Base {
+	/**
+	 * Contains all errors
+	*/
 	static $errors = array();
 	/**
-	 * description
-	 * @access	public
-	 * @param		int			number	Error number
-	 * @param		string	message	Error message
-	 * @param		string	file		In file $file
-	 * @param		int			line		In line $line
-	 * @return	void
+	 * Adds error to $errors
+	 *
+	 * @access public
+	 * @param  int    number  Error number
+	 * @param  string message Error message
+	 * @param  string file    In file $file
+	 * @param  int    line    In line $line
+	 * @return void
+	 * @static
+	 * @todo   Avoid switch
 	 */
 	static function error( $number, $message, $file, $line ) {
 		if( error_reporting() ) {
@@ -53,12 +59,20 @@ class Errors extends Base {
 			if( $number === E_USER_ERROR || $number == E_ERROR ) self::show();
 		}
 	}
-	
+
+	/**
+	 * Shows error page
+	 *
+	 * @access public
+	 * @return return
+	 * @static
+	 */
 	static function show() {
 		if( empty( self::$errors ) ) return;
 		@ob_clean();
-		header( "HTTP/1.1 500 Internal Server Error" ); // (?)
-		global $errors; $errors = self::$errors;
+		header( "HTTP/1.1 500 Internal Server Error" );
+
+		$errors = self::$errors;
 		include PUBLIC_DIR . "500.php";
 		exit;
 	}
