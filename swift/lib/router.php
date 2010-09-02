6941f1e6a79df3500a4af6e2c73ecbc701af0144
Matija Folnovic <matijafolnovic@gmail.com>
2010-09-02 13:41:29 +0200

    Better docs - router

--- swift/lib/router.php
+++ swift/lib/router.php
@@ -3,38 +3,62 @@
 /**
  * Swift
  *
- * @package		Swift
- * @author		Swift dev team
- * @copyright	Copyright (c) 2010, Swift dev team
- * @license		LICENSE
+ * @author    Swift dev team
+ * @copyright Copyright (c) 2010, Swift dev team
+ * @license   LICENSE
+ * @package   Swift
  */
 
 /**
  * Swift Router
  *
- * Routes all requests to correct controller & action
+ * Routes all requests to correct controller and action
  *
- * @package			Swift
- * @subpackage	Router
- * @author			Swift dev team
+ * @author     Swift dev team
+ * @package    Swift
+ * @subpackage Router
  */
 
 class Router extends Base {
+	/**
+	 * Contains all routes
+	 */
 	var $routes;
+	/**
+	 * Current path
+	 */
 	var $path;
+	/**
+	 * Root route
+	 * Use this route for empty url
+	 */
 	var $root;
+	/**
+	 * @internal
+	 *
+	 * If this one is true, then try to find other route
+	 * Signal from controller that Router missed correct route
+	*/
 	var $continueRouting;
 
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 * @param  string name description
+	 * @return return
+	 */
 	function __construct() {
 		$this -> loadRoutes();
 	}
 
 	/**
 	 * Main function responsible to route $path to current controller & action
-	 * @access	public
-	 * @param		string	path					Path to route from
-	 * @return	void
-	 * @todo		Allows URLs like %controller%/%action%/%h%:%m%:%s%
+	 *
+	 * @access public
+	 * @param  string $path Path to route from
+	 * @return void
+	 * @todo   Allows URLs like %controller%/%action%/%h%:%m%:%s%
 	 */
 	function route( $path ) {
 		if( empty( $path ) ) {
@@ -67,12 +91,11 @@ class Router extends Base {
 
 	/**
 	 * Checks if route $route is correct for $path
-	 * @access	public
-	 * @param		array	route	Route to check for
-	 * @param		array	path	Current path
-	 * @param		bool	runController	Does router need to run it too?
-	 * @return	return
-	 * @todo		Remove runController
+	 *
+	 * @access public
+	 * @param  array $route Route to check for
+	 * @param  array $path  Current path
+	 * @return return
 	 */
 	function checkRoute( &$route, $path ) {
 		$ret = array();
@@ -98,17 +121,21 @@ class Router extends Base {
 		return !$this -> continueRouting;
 	}
 
+	/**
+	 * Load all routes from routes.php
+	 */
 	function loadRoutes() {
 		include CONFIG_DIR . "routes.php";
 	}
 
 	/**
 	 * Parses route $route with options $options and adds it to other routes
-	 * @access	public
-	 * @param		string	route		Route to parse and add
-	 * @param		array		options	Options for this route
-	 * @return	void
-	 * @todo		Do I really need to parse all routes?
+	 *
+	 * @access public
+	 * @param  string $route   Route to parse and add
+	 * @param  array  $options Options for this route
+	 * @return void
+	 * @todo   Do I really need to parse all routes?
 	 */
 	function addRoute( $route, $options = array() ) {
 		$ret = array();
@@ -125,16 +152,14 @@ class Router extends Base {
 
 	/**
 	 * Creates root route, for empty url
-	 * @access	public
-	 * @param		string	controller	Root controller
-	 * @param		string	action			Root action
-	 * @return	void
+	 * @access public
+	 * @param  string $controller Root controller
+	 * @param  string $action     Root action
+	 * @return void
 	 */
 	function root( $controller, $action ) {
 		$this -> root = array( 'controller' => $controller, 'action' => $action );
 	}
 }
-
-$router = new Router;
 
 ?>
