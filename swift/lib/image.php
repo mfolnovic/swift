813b8b3c93dd99b219fa0aa82e7ff9fad565a47a
Matija Folnovic <matijafolnovic@gmail.com>
2010-09-02 14:46:18 +0200

    better docs - images

--- swift/lib/image.php
+++ swift/lib/image.php
@@ -3,10 +3,10 @@
 /**
  * Swift
  *
- * @package		Swift
- * @author		Swift dev team
- * @copyright	Copyright (c) 2010, Swift dev team
- * @license		LICENSE
+ * @author    Swift dev team
+ * @copyright Copyright (c) 2010, Swift dev team
+ * @license   LICENSE
+ * @package   Swift
  */
 
 /**
@@ -14,51 +14,54 @@
  *
  * Provides image manipulation like cropping, resizing etc. to Swift
  *
- * @package			Swift
- * @subpackage	Image
- * @author			Swift dev team
+ * @author     Swift dev team
+ * @package    Swift
+ * @subpackage Image
  */
 
 
 class Image extends Base {
-	var $image, $name, $dir, $width, $height;
+	var $image;
+	var $name;
+	var $dir;
+	var $width;
+	var $height;
 
 	/**
 	 * Constructor
-	 * @access	public
-	 * @param		string	path	Path to image
-	 * @return	void
+	 * @access public
+	 * @param  string $path Path to image
+	 * @return void
 	 */
 	function __construct( $path ) {
-		$this -> dir = dirname( $path ) . '/';
-
-		$this -> image = new Imagick( $path );
-		$this -> width = $this -> image -> getImageWidth();
+		$this -> dir = realpath( $path ) . '/';
+		$this -> image  = new Imagick( $path );
+		$this -> width  = $this -> image -> getImageWidth();
 		$this -> height = $this -> image -> getImageHeight();
 	}
 
 	/**
 	 * Write current image to $name
-	 * @access	public
-	 * @param		string	name	Name of new image
-	 * @return	void
+	 * @access public
+	 * @param  string $name Name of new image
+	 * @return void
 	 */
 	function write( $name ) {
 		$this -> image -> writeImage( $this -> dir . $name );
-		
+
 		return $this;
 	}
 
 	/**
 	 * Resize image proportionally, and then crop what's redundant
-	 * @access	public
-	 * @param		int	width	Width of new image
-	 * @param		int	height	Height of new image
-	 * @return	object
+	 * @access public
+	 * @param  int $width Width of new image
+	 * @param  int $height Height of new image
+	 * @return object
 	 */
 	function resizeAndCrop( $width, $height ) {
 		$this -> image -> cropThumbnailImage( $width, $height );
-		
+
 		return $this;
 	}
 }
