da01c9e4882b438b8270462d7eb7b072103a102b
Matija Folnovic <matijafolnovic@gmail.com>
2010-10-09 21:17:08 +0200

    fix app for changes before

--- app/public/index.php
+++ app/public/index.php
@@ -1,10 +1,16 @@
 <?php
 
-define( 'ENV', 'development' );
-define( 'ROOT_DIR', realpath( getcwd() . '/../..' ) . '/' );
-define( 'DIR', ROOT_DIR );
-define( 'LIB_DIR', ROOT_DIR . 'swift/lib/' );
+define('ENV', 'development');
+define('ROOT_DIR', realpath(getcwd() . '/../..') . '/');
+define('LIB_DIR', ROOT_DIR . 'swift/lib/');
 
-include LIB_DIR . 'boot.php';
+include LIB_DIR . "base.php";
+include LIB_DIR . "dir.php";
+include LIB_DIR . "constants.php";
+include LIB_DIR . "errors.php";
+include LIB_DIR . "helpers.php";
+include LIB_DIR . 'app.php';
+
+App::boot();
 
 ?>
